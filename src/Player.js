d79965ccb0969b2ec3b6145419ab5e5f11202322
David Goemans <dgoemans@gmail.com>
2015-09-02 20:05:49 +0200

    - Animations!

--- src/Player.js
+++ src/Player.js
@@ -1,6 +1,6 @@
 function Player(x,y)
 {
-    Sprite.call(this, x, y, 'assets/player.png');
+    Sprite.call(this, x, y, 'assets/player.png', 2, 2);
     this.physics = true;
 
     this.walkSpeed = 300;
@@ -13,23 +13,34 @@ Player.prototype = Object.create(Sprite.prototype);
 
 Player.prototype.move = function(dir)
 {
-    this.moveDirX = dir;
+    if(!this.inputLocked)
+    {
+        this.moveDirX = dir;
+        this.play(0, true, 10);
+    }
 };
 
 Player.prototype.die = function()
 {
     this.inputLocked = true;
+    this.stop();
 }
 
 Player.prototype.stop = function()
 {
+    this.velocity.x = 0;
     this.moveDirX = 0;
+    Sprite.prototype.stop.call(this);
 };
 
 Player.prototype.jump = function()
 {
+
     if(!this.inputLocked && (this.colliding.bottom || this.colliding.top))
+    {
+        this.play(1, false);
         this.velocity.y = -1300 * Math.sign(this.gravity);
+    }
 };
 
 Player.prototype.flip = function()
@@ -45,6 +56,12 @@ Player.prototype.update = function(deltaSeconds)
         if(this.moveDirX !== 0)
             this.flipX = this.moveDirX < 0;
         this.velocity.x = this.moveDirX * this.walkSpeed;
+
+        if(this.animation === 1 && this.velocity.y > 0)
+        {
+            Sprite.prototype.stop.call(this);
+            this.frame = 0;
+        }
     }
 
     Sprite.prototype.update.call(this, deltaSeconds);
